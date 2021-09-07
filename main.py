9694f0101606995767d101ec5ce5c20c353c72e1
ericocsrodrigues <43556484+ericocaina@users.noreply.github.com>
2021-09-06 22:37:48 -0300

    Realizado ajuste no tamanho das fontes e dos botões da janela de login.

---
+++ main.py
@@ -0,0 +1,8 @@
+from PySimpleGUI import PySimpleGUI as sg
+import telas
+
+
+telas.janela_login()
+
+while True:
+
--- telas.py
+++ telas.py
@@ -1,19 +1,25 @@
-from PySimpleGUI import PySimpleGUI  as sg
+from PySimpleGUI import PySimpleGUI as sg
 
 
 def janela_login():
     sg.theme('LightGray1')
     layout = [
-        [sg.Text('Login:'), sg.Input(key='login', size=(31, 1))],
-        [sg.Text('Senha:'), sg.Input(key='senha', password_char='×', size=(30, 1))],
-        [sg.Text('')],
-        [sg.Button('Entrar'), sg.Button('Novo Cadastro'), sg.Button('Sair')],
+        [sg.Text('Login:', font=('Arial', 12), size=(5, 1)), sg.Input(key='login', size=(30, 1))],
+        [sg.Text('Senha:', font=('Arial', 12), size=(5, 1)), sg.Input(key='senha', password_char='×', size=(30, 1))],
+        [sg.Text('\n')],
+        [sg.Button('Entrar', size=(8, 2)), sg.Button('Novo Cadastro', size=(8, 2)), sg.Button('Sair', size=(8, 2))],
     ]
-    return sg.Window('Tela de Login', layout=layout, finalize=True)
+    return sg.Window('Tela de Login', layout=layout, size=(400, 300), finalize=True,
+                     element_justification='center', resizable=True)
+    # element_justification server para centralizar o layout
+    # resizable serve para permitir que a janela seja redimensionada após a axecursão
+
+
 
 
 if __name__ == '__main__':
-    janela_login()
+    janela1, janela2 = janela_login(), None
     while True:
-        events, values = sg.read_all_windows()
+        windwos, eventos, valores = sg.read_all_windows()
+
 
