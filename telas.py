371263a1123f3f3e90bd0973c2bcaf7dc36b4114
ericocsrodrigues <43556484+ericocaina@users.noreply.github.com>
2021-09-09 18:55:23 -0300

    Realizado pequenos ajustes, de tamanho de fonte e barras de inpput.

--- telas.py
+++ telas.py
@@ -5,7 +5,8 @@ def janela_login():
     sg.theme('LightGray1')
     layout = [
         [sg.Text('Login:', font=('Arial', 12), size=(5, 1)), sg.Input(key='login', size=(30, 1))],
-        [sg.Text('Senha:', font=('Arial', 12), size=(5, 1)), sg.Input(key='senha', password_char='×', size=(30, 1))],
+        [sg.Text('Senha:', font=('Arial', 12), size=(5, 1)), sg.Input(key='senha', password_char='*',
+                                                                      size=(30, 1), text_color='red')],
         [sg.Text('\n')],
         [sg.Button('Entrar', size=(8, 2)), sg.Button('Novo Cadastro', size=(8, 2)), sg.Button('Sair', size=(8, 2))],
     ]
@@ -17,12 +18,12 @@ def janela_login():
 def janela_cadastro():
     sg.theme('DarkGrey14')
     layout = [
-        [sg.Text('Nome:       '), sg.Input(key='nome', size=(40, 1))],
-        [sg.Text('Sobre Nome: '), sg.Input(key='sobre_nome', size=(40, 1))],
-        [sg.Text('Email:      '), sg.Input(key='email', size=(40, 1))],
-        [sg.Text('Telefone    '), sg.Input(key='telefone')],
-        [sg.Text('Login:      '), sg.Input(key='login')],
-        [sg.Text('Senha:      '), sg.Input(password_char='×', key='nova_senha')],
+        [sg.Text(f'{"Nome":<10}'), sg.Input(key='nome', size=(100, 1))],
+        [sg.Text(f'{"Sobre Nome":<10}'), sg.Input(key='sobre_nome', size=(100, 1))],
+        [sg.Text(f'{"Email":<10}'), sg.Input(key='email', size=(100, 1))],
+        [sg.Text(f'{"Telefone":<10}'), sg.Input(key='telefone', size=(100, 1))],
+        [sg.Text(f'{"Login":<10}'), sg.Input(key='login', size=(100, 1))],
+        [sg.Text(f'{"Senha":<10}'), sg.Input(password_char='*', key='nova_senha', size=(100, 1))],
         [sg.Text(' ')],
         [sg.Button('Salvar', size=(10, 3)), sg.Button('Cancelar', size=(10, 3))],
     ]
